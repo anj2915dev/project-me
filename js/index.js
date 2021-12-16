016c66d653c2842d884692c8df9808b16b62ed1e
John Doe <alirezanjprogrammer110@gmail.com>
2021-12-16 06:46:26 +0330

    respansave and about me and style

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const btnMenu=document.querySelector(".bx-menu");
+const menu=document.querySelector(".menuandprof")
+btnMenu.addEventListener("click",(e)=>{
+    menu.style.transform="translate(0px)";
+
+})
+
